9dafc7a79609ffc6e03e9e62b821c7874e1d59fd
Catherine Leveille <leveillec@hotmail.ca>
2020-11-27 09:45:48 -0500

    scripts en classe

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded",function(){
+let connexion= new MovieDB();
+
+connexion.requeteDernierFilm();
+})
+
+class MovieDB{
+
+    constructor() {
+        console.log("constructeur");
+
+        this.APIkey="8ec3413c6e72c2abaf8cc8645163e2b6";
+
+        this.lang="fr-CA"
+
+        this.baseURL="https://api.themoviedb.org/3";
+
+        this.imgPath="https://image.tmdb.org/t/p/";
+
+        this.totalFilm=8;
+    }
+    requeteDernierFilm(){
+
+        let requete = new XMLHttpRequest();
+
+        requete.addEventListener("loadend", this.retourRequeteDernierFilm.bind(this));
+
+        //requete.open("GET","https://api.themoviedb.org/3/movie/now_playing?api_key=8ec3413c6e72c2abaf8cc8645163e2b6&language=fr-CA&page=1");
+        requete.open("GET",this.baseURL + "/movie/now_playing?api_key=" + this.APIkey + "&language=" + this.lang + "&page=1");
+
+        requete.send();
+    }
+    retourRequeteDernierFilm(e){
+        console.log("retour dernier film") ;
+
+    let target= e.currentTarget;
+    let data;
+
+
+        data= JSON.parse(target.responseText).results;
+        //sert à rendre le text plus lisible
+        console.log(data);
+
+
+    }
+
+}
